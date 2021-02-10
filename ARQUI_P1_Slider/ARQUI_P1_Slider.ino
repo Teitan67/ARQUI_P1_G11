48c945363e26f74ec42168c2cc5a7dd664de8ab3
Oscar Leon <Oskrr67@gmail.com>
2021-02-10 17:15:28 -0600

    Agregando animaciones de matriz

--- ARQUI_P1_Slider/ARQUI_P1_Slider.ino
+++ ARQUI_P1_Slider/ARQUI_P1_Slider.ino
@@ -12,6 +12,7 @@
 #define CS_PIN    10
 int direc_pin =8;
 int vel_pin=9;
+int msm_pin=7;
 
 MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
@@ -26,9 +27,10 @@ uint16_t scrollPause = 1000; // en milisegundos
 // Global message buffers shared by Serial and Scrolling functions
 #define  BUF_SIZE  75
 char curMessage[BUF_SIZE] = { "" };
-char newMessage[BUF_SIZE] = { "TP1 - GRUPO 11 - SECCION B" };
+char newMessage[BUF_SIZE] = { "*TP1 - GRUPO 11 - SECCION B*" };
 bool newMessageAvailable = true;
-
+int  direccionAnterior=1;
+int modoAnterior=1;
 
 void readSerial(void)
 {
@@ -47,33 +49,8 @@ void readSerial(void)
       cp++;
   }
 }
-
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Iniciando aplicacion con normalidad...\n");
-
-  P.begin();
-  P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
-  pinMode(direc_pin,INPUT);
-  pinMode(vel_pin,INPUT);
-}
-
-void loop() {
-  if (digitalRead(direc_pin)==1){
-    Serial.println("Cambio de direccion!!");
-    if(scrollEffect==PA_SCROLL_RIGHT){
-      scrollEffect = PA_SCROLL_LEFT; 
-      scrollAlign = PA_LEFT;
-    }else{
-      scrollEffect = PA_SCROLL_RIGHT;
-      scrollAlign = PA_RIGHT;
-      
-    }
-    P.displayClear();// limpia las matrices y se reinicia la animación
-    P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
-    delay(500);
-  }
-  if (digitalRead(vel_pin)==1){
+void cambioVelocidad(){
+    if (digitalRead(vel_pin)==1){
     Serial.println("Cambio de Velocidad!!");
     if(scrollSpeed==30){
       scrollSpeed=100;
@@ -83,15 +60,77 @@ void loop() {
     P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
     delay(500);
   }
-  if (P.displayAnimate())
-  {
-    if (newMessageAvailable)
-    {
-      strcpy(curMessage, newMessage);
-      newMessageAvailable = false;
-    }
-    P.displayReset();
-  }
-  readSerial();
 
+}
+
+void cambioDireccion(){
+  if (digitalRead(direc_pin)==1&&direccionAnterior==0){
+    Serial.println("Cambio de direccion!!");
+    direccionAnterior=1;
+    scrollEffect = PA_SCROLL_RIGHT; 
+    scrollAlign = PA_RIGHT;
+    P.displayClear();// limpia las matrices y se reinicia la animación
+    P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+    delay(200);
+  } 
+  if (digitalRead(direc_pin)==0&&direccionAnterior==1){
+    Serial.println("Cambio de direccion!!");
+    direccionAnterior=0;
+    scrollEffect = PA_SCROLL_LEFT; 
+    scrollAlign = PA_LEFT;
+    P.displayClear();// limpia las matrices y se reinicia la animación
+    P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+    delay(200);
+  } 
+  
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Iniciando aplicacion con normalidad...\n");
+
+  P.begin();
+  P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+  pinMode(direc_pin,INPUT);
+  pinMode(vel_pin,INPUT);
+  pinMode(msm_pin,INPUT);
+}
+
+void loop() {
+
+  modoDisplay();
+
+
+}
+void modoDisplay(){
+    if(digitalRead(msm_pin)==0&&modoAnterior==1){
+        Serial.println("Modo scrolling");
+        modoAnterior=0;
+        P.displayClear();
+        P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+    }
+    if(digitalRead(msm_pin)==0&&modoAnterior==0){
+        cambioVelocidad();
+        cambioDireccion();
+        if (P.displayAnimate())
+        {
+          if (newMessageAvailable)
+          {
+            strcpy(curMessage, newMessage);
+            newMessageAvailable = false;
+          }
+            P.displayReset();
+        }
+        readSerial();  
+    }
+    if(digitalRead(msm_pin)==1&&modoAnterior==0){
+      Serial.println("Modo letra por letra");
+      modoAnterior=1;
+      P.displayClear();
+    }
+    if(digitalRead(msm_pin)==1&&modoAnterior==1){
+      P.setTextEffect(PA_NO_EFFECT ,PA_NO_EFFECT);
+      P.setTextBuffer("prueba");
+      P.displayAnimate();
+    }
 }
